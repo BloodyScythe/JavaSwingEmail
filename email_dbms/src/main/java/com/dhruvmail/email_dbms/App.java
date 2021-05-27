c7ec038450932cf415b5d5c61d95affbe8f524c8
Dhruv kaith <60250508+BloodyScythe@users.noreply.github.com>
2021-05-27 14:59:18 +0530

    Add files via upload
    merged from master

---
+++ email_dbms/src/main/java/com/dhruvmail/email_dbms/App.java
@@ -0,0 +1,22 @@
+package com.dhruvmail.email_dbms;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App extends Page implements  ActionListener {
+
+	public static void main(String[] args) {
+		
+		Page testpage = new Page();
+		//send.addActionListener(this);
+		//System.out.println(testpage.clientmail);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		//if(e.getSource()==send) {
+		//System.out.println(testpage.clientmail);
+	 }
+	
+	}
